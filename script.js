34a33f1d3aaf2f990f5e8d6c4a4c05f448cf08c7
govarthan332005 <govarthanreddyy@gmail.com>
2024-12-19 23:02:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.querySelector('.material-symbols-outlined').addEventListener('click', (
     const navLinks = document.querySelector('.side');
     navLinks.classList.toggle('active');
   });
+  function video() {
+      // Open another HTML file in a new blank tab
+      window.open('video.html', '_blank'); // Replace 'video.html' with your actual file path
+  }
